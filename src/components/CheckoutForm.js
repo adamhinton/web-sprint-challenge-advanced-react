2f08cd58b195358fae6a8421f6f92389a716704a
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-03 12:19:31 -0500

    working on building useForm. Haven't made any functional changes to the site yet.

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -9,12 +9,12 @@ const initialValue = {
   zip: "",
 };
 
-// This form should be handled by a "useForm" custom hook
+// This form should be handled by a "useForm" custom hook.
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false); 
   const [values, setValues] = useState(initialValue);
 
   const handleChanges = (e) => {
