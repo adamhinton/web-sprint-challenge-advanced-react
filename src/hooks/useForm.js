4290364676b96691e5d02ef5b2c85449b0395f48
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-03 13:35:22 -0500

    done with comments and cleaning up code. Now for interview questions.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,10 +6,13 @@ const useForm = (initialValue) =>{
     
     const [showSuccessMessage, setShowSuccessMessage] = useState(false); 
 
+    //not totally sure I was supposed to put handleChanges in here but it works.
     const handleChanges = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
       };
 
+
+        //I don't think it's right to put both showSuccessMessage and setShowSuccessMessage in here, but I couldn't get it to work any other way. If I only put in showSuccessMessage then set... would be undefined, and if I only put in set... then show... would be undefined.
       return [values, handleChanges, showSuccessMessage, setShowSuccessMessage]
 }
 
