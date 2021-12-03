d843ae0139257c1444deb1318e4ff79600aa5755
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-03 13:44:34 -0500

    all done now

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -7,23 +7,25 @@ import userEvent from "@testing-library/user-event";
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("renders without errors", () => {
+        //making sure the component renders 
     render(<CheckoutForm/>);
 });
 
 test("shows success message on submit with form details", () => {
+    //making sure the component renders 
     render(<CheckoutForm/>);
 
-    const firstNameField = screen.getByLabelText(/first name:/i);
-    // console.log(firstNameField);
 
-        // userEvent.type(firstNameField, 'blahblah');
+    /declaring my variables, this is all the different fields then the checkout button
+    const firstNameField = screen.getByLabelText(/first name:/i);
     const lastNameField =  screen.getByLabelText(/last name:/i)
     const  addressField =  screen.getByLabelText(/address:/i)
     const cityField  =  screen.getByLabelText(/city:/i)
     const  stateField =  screen.getByLabelText(/state:/i)
-    const  zipField =  screen.getByLabelText(/zip:/i)
+    const  zipField =  screen.getByLabelText(/zip:/i);
     const submitBtn = screen.getByRole("button");
 
+    //these expects are making sure that the fields exist, I commented them out for the end result.
     // expect(firstNameField).toBeTruthy();
     // expect(lastNameField).toBeTruthy();
     // expect(addressField).toBeTruthy();
@@ -32,6 +34,8 @@ test("shows success message on submit with form details", () => {
     // expect(zipField).toBeTruthy();
     // expect(submitBtn).toBeTruthy();
 
+
+    //filling in all the fields
     userEvent.type(firstNameField, 'Adam')
     userEvent.type(lastNameField, 'Hinton')
     userEvent.type(addressField, '3223 Wallace St 1R')
@@ -39,9 +43,12 @@ test("shows success message on submit with form details", () => {
     userEvent.type(stateField, 'Pennsylvania')
     userEvent.type(zipField, '19104')
 
+    //hitting the buttin to checkout
     userEvent.click(submitBtn);
 
+    //declaring a variable for the success message
     const successMsg = screen.getByText(/You have ordered some plants! Woo-hoo!/i);
 
+    //making sure the success message exists
     expect(successMsg).toBeTruthy();
 });
