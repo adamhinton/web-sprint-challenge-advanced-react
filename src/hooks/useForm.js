3ea1c657b8a4fa71e5b8586c9180dec20a720beb
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-03 12:30:15 -0500

    have added values and handlechanges to the custom hook, but running in to problems with showSuccessMessage

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,6 +1,5 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react";
-import { useForm } from "react-hook-form";
 
 const useForm = (initialValue) =>{
     const [values, setValues] = useState(initialValue);
@@ -9,9 +8,9 @@ const useForm = (initialValue) =>{
         setValues({ ...values, [e.target.name]: e.target.value });
       };
 
-      const [showSuccessMessage, setShowSuccessMessage] = useState(false); 
+    //   const [showSuccessMessage, setShowSuccessMessage] = useState(false); 
 
-      return [values, handleChanges, showSuccessMessage]
+      return [values, handleChanges]
 }
 
  
